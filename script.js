847092cf8d149199bdc6df56599cf38bfd06b7f2
vlapav13-eng <vlapav13@gmail.com>
2025-12-04 23:34:03 +0200

    Implement playTripleBeep function for sound alert
    Adds a function to play a triple beep sound.

---
+++ script.js
@@ -0,0 +1,10 @@
+// Тройной звуковой сигнал
+function playTripleBeep() {
+    const beepData = "data:audio/wav;base64,UklGRiQAAABXQVZFZm10IBAAAAABAAEAgD4AAAB9AAACABAAZGF0YQgAAAABAQEB";
+    const beep = new Audio(beepData);
+
+    // Проигрываем 3 раза с задержкой
+    beep.play();
+    setTimeout(() => new Audio(beepData).play(), 250); 
+    setTimeout(() => new Audio(beepData).play(), 500);
+}
